59ddda0ca7a39c4d253290f214fef9aac055b438
vivianz1 <vivianzhu120@gmail.com>
2023-05-18 10:54:46 -0400

    create polynomial and starting

---
+++ Polynomial.java
@@ -0,0 +1,14 @@
+public class Polynomial {
+    double[] coefficients;
+
+    public Polynomial() {
+        coefficients[] = 0;
+    }
+
+    public Polynomial( ) {
+
+    }
+
+    public 
+    
+}
