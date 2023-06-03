d3483d56ed9cf0bfb3d2ca7b246c6d7621eb274b
vivianz1 <vivianzhu120@gmail.com>
2023-06-03 15:56:40 -0400

    added Polynomial.java and Driver.java

--- Polynomial.java
+++ Polynomial.java
@@ -2,13 +2,36 @@ public class Polynomial {
     double[] coefficients;
 
     public Polynomial() {
-        coefficients[] = 0;
+        coefficients = new double[1];
+        coefficients[0] = 0;
     }
 
-    public Polynomial( ) {
-
+    public Polynomial(double[] numbers) {
+        coefficients = numbers;
     }
 
-    public 
-    
+    public Polynomial add(Polynomial p) {
+        int big = 0;
+        if (coefficients.length > p.coefficients.length) big = coefficients.length;
+        else big = p.coefficients.length;
+        double[] newco = new double[big];
+        for (int i = 0; i < big; i++) {
+            if (i >= coefficients.length) newco[i] = p.coefficients[i];
+            else if (i >= p.coefficients.length) newco[i] = coefficients[i];
+            else newco[i] = coefficients[i] + p.coefficients[i];
+        }
+        return new Polynomial(newco);
+    }
+
+    public double evaluate(double x) {
+        double sum = coefficients[0];
+        for (int i = 1; i < coefficients.length; i++) {
+            sum += coefficients[i]*Math.pow(x, i);
+        }
+        return sum;
+    }
+
+    public boolean hasRoot(double y) {
+        return evaluate(y) == 0;
+    }
 }
